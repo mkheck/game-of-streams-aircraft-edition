2b7c0fed9ab81387090713f3f5e9d890d63b9481
Mark Heckler <mark.heckler@gmail.com>
2020-06-29 20:41:11 -0500

    Added sink service repo

---
+++ plane-sink/src/main/java/com/thehecklers/planesink/PlaneSinkApplication.java
@@ -0,0 +1,42 @@
+package com.thehecklers.planesink;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+@SpringBootApplication
+public class PlaneSinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneSinkApplication.class, args);
+	}
+
+}
+
+@Configuration
+class PlaneLogger {
+/*
+	@Bean
+	Consumer<Flux<EssentialAircraft>> logIt() {
+		return aircraftFlux -> aircraftFlux.subscribe(System.out::println);
+	}
+*/
+
+	@Bean
+	Consumer<List<EssentialAircraft>> logIt() {
+		return eACs -> eACs.forEach(System.out::println);
+	}
+}
+
+@Data
+@AllArgsConstructor
+class EssentialAircraft {
+	private final String callsign, reg, type, mfr;
+}
---
+++ plane-sink/src/test/java/com/thehecklers/planesink/PlaneSinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.planesink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneSinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
