eae30d30fb9d6f7e66e56ce3f88738f5f5fa0c0b
Mark Heckler <mark.heckler@gmail.com>
2020-07-02 13:43:19 -0500

    Updated Source service

--- plane-source/src/main/java/com/thehecklers/planesource/PlaneSourceApplication.java
+++ plane-source/src/main/java/com/thehecklers/planesource/PlaneSourceApplication.java
@@ -5,17 +5,11 @@ import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.function.context.PollableBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class PlaneSourceApplication {
@@ -30,32 +24,31 @@ public class PlaneSourceApplication {
 class PositionFeeder {
 	private final WebClient client = WebClient.create("http://localhost:8080");
 
-/*
 	@PollableBean
 	Supplier<Flux<Aircraft>> sendAC() {
 		return () -> client.get()
 				.retrieve()
 				.bodyToFlux(Aircraft.class)
-				.filter(aircraft -> !aircraft.getReg().isEmpty());
+				.filter(aircraft -> !aircraft.getReg().isEmpty())
+				.log();
 	}
-*/
 
+/*
 	@Bean
 	Supplier<List<Aircraft>> sendAC() {
 		return () -> {
-			List<Aircraft> aircrafts = client.get()
+			List<Aircraft> aircraftList = client.get()
 					.retrieve()
 					.bodyToFlux(Aircraft.class)
 					.filter(aircraft -> !aircraft.getReg().isEmpty())
 			.toStream().collect(Collectors.toList());
 
-			/*for (Aircraft ac: aircrafts) {
-				System.out.println(ac);
-			}*/
+			//aircraftList.forEach(System.out::println);
 
-			return aircrafts;
+			return aircraftList;
 		};
 	}
+*/
 }
 
 @Data
